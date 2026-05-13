62f00eafa20219d232f387221fb7ce958ecac21b
Alisher <alisherkaman7@gmail.com>
2026-05-13 13:20:13 +0500

    fix: transfer admin rights from deployer to Timelock

--- script/DeployProtocol.s.sol
+++ script/DeployProtocol.s.sol
@@ -7,51 +7,48 @@ import {Timelock} from "../src/Timelock.sol";
 import {ProtocolGovernor} from "../src/ProtocolGovernor.sol";
 import {AMMPair} from "../src/AMMPair.sol";
 import {YieldVault} from "../src/YieldVault.sol";
-import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {PriceOracle} from "../src/PriceOracle.sol";
 import {IVotes} from "openzeppelin-contracts/contracts/governance/utils/IVotes.sol";
-
-// Создаем тестовый стейблкоин для второй половины пула AMM
-contract MockUSD is ERC20 {
-    constructor() ERC20("Mock USD", "mUSD") {}
-    function mint(address to, uint256 amount) public { _mint(to, amount); }
-}
 
 contract DeployProtocol is Script {
     function run() external {
-        // Стандартный приватный ключ для локальной тестовой сети Anvil
         uint256 deployerPrivateKey = vm.envOr("PRIVATE_KEY", uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80));
+        address deployer = vm.addr(deployerPrivateKey);
 
         vm.startBroadcast(deployerPrivateKey);
 
-        // 1. Деплоим токен управления
+        // 1. Деплоим токены
         GovToken govToken = new GovToken();
-        console.log("1. GovToken deployed at:", address(govToken));
-
-        // 2. Деплоим Timelock (задержка 2 дня)
+        
+        // 2. Деплоим Timelock
+        // Сначала админом будет деплоер, чтобы правильно настроить роли
         address[] memory proposers = new address[](0);
         address[] memory executors = new address[](0);
-        Timelock timelock = new Timelock(172800, proposers, executors, msg.sender);
-        console.log("2. Timelock deployed at:", address(timelock));
+        Timelock timelock = new Timelock(172800, proposers, executors, deployer);
 
         // 3. Деплоим DAO
         ProtocolGovernor governor = new ProtocolGovernor(IVotes(address(govToken)), timelock);
-        console.log("3. ProtocolGovernor deployed at:", address(governor));
 
-        // Выдаем права DAO
-        timelock.grantRole(timelock.PROPOSER_ROLE(), address(governor));
-        timelock.grantRole(timelock.EXECUTOR_ROLE(), address(0));
+        // 4. Настройка ролей в Timelock
+        bytes32 proposerRole = timelock.PROPOSER_ROLE();
+        bytes32 executorRole = timelock.EXECUTOR_ROLE();
+        bytes32 adminRole = timelock.DEFAULT_ADMIN_ROLE();
 
-        // 4. Деплоим второй токен для пула (mUSD)
-        MockUSD mUsd = new MockUSD();
-        console.log("4. MockUSD deployed at:", address(mUsd));
-
-        // 5. Деплоим AMM пулл (GovToken / mUSD)
-        AMMPair amm = new AMMPair(address(govToken), address(mUsd));
-        console.log("5. AMMPair deployed at:", address(amm));
-
-        // 6. Деплоим YieldVault
+        timelock.grantRole(proposerRole, address(governor)); // Только DAO может предлагать
+        timelock.grantRole(executorRole, address(0));      // Кто угодно может исполнять после задержки
+        
+        // ВАЖНО: Деплоер отказывается от прав админа. Теперь протоколом управляет только DAO
+        timelock.revokeRole(adminRole, deployer); 
+        
+        // 5. Деплоим AMM и Vault
+        AMMPair amm = new AMMPair(address(govToken), address(0)); // Адрес второго токена пока 0
         YieldVault vault = new YieldVault(govToken);
-        console.log("6. YieldVault deployed at:", address(vault));
+        
+        // 6. Оракул для Sepolia
+        PriceOracle oracle = new PriceOracle(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        console.log("Governor deployed at:", address(governor));
+        console.log("Timelock deployed at:", address(timelock));
 
         vm.stopBroadcast();
     }
