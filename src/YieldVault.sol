52bc8678455f9e2ba101a6b0d4a963b3786d4d44
Alisher <alisherkaman7@gmail.com>
2026-05-22 16:00:15 +0500

    style: format solidity contracts with forge fmt

--- src/YieldVault.sol
+++ src/YieldVault.sol
@@ -6,8 +6,5 @@ import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ERC4626} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC4626.sol";
 
 contract YieldVault is ERC4626 {
-    
     constructor(IERC20 asset_) ERC4626(asset_) ERC20("DeFi Super Vault", "vDST") {}
-
-
 }
